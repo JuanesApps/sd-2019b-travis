4a2e60d4c5f16dade5df1590082f4a437f312a65
juan.quinayas <juanes-2504@hotmail.com>
2019-10-02 19:20:57 -0500

    add some text 2

--- test_demo.py
+++ test_demo.py
@@ -10,3 +10,13 @@ def test_add():
     assert demo.add(3,4)==7
     assert demo.add(1,9)==10
     assert demo.add(2,4)==6
+
+def test_sub():
+    # GIVEN two numbers
+    # WHEN I invoke the method
+    # THEN I get the addition of two numbers
+    #add_result = demo.add(3,4)
+    #assert add_result == 7
+    assert demo.sub(3,4) == -1
+    assert demo.sub(1,9) == -8
+    assert demo.sub(2,4) == -2
